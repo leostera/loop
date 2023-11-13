246800ee062d9f5a7bfb7a61a426b0faa401b1da
Leandro Ostera <leandro@abstractmachines.dev>
2023-11-13 02:21:52 +0100

    docs: document interface

--- lib/loop.mli
+++ lib/loop.mli
@@ -1,3 +1,12 @@
 val loop : (unit -> unit) -> unit
+(** `loop fn` will loop the `fn` function infinitely.
+
+    If you want to break the loop, or skip an iteration, call `break ()` or
+    `continue ()` within it.
+*)
+
 val break : unit -> unit
+(** `break ()` breaks out of a loop. *)
+
 val continue : unit -> unit
+(** `continue ()` skips the rest of the current loop iteration and begins the next iteration. *)
